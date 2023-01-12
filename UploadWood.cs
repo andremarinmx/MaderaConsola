7cefa1128edc431682e7c8fe4f4ee4de0558fc39
Andre Marin <jmarin35@jci.com>
2023-01-11 18:50:24 -0700

    Preparación para subir a macpac

---
+++ UploadWood.cs
@@ -0,0 +1,40 @@
+﻿using IBM.Data.DB2.iSeries;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaderaConsola
+{
+    class UploadWood
+    {
+        static iDB2Connection conAS400;
+        //static iDB2DataReader dataTable;
+
+        public UploadWood()
+        {
+            string User = "bdcadmin";
+            string Pass = "ruskin";
+
+            try
+            {
+                conAS400 = new iDB2Connection("DataSource = 10.46.144.70;UserID =" + User + "; Password =" + Pass + "; DataCompression = True;");
+                conAS400.Open();
+            }
+            catch
+            {
+                return;
+            }
+        }
+        public void OpenSalesQry(string orderNumber)
+        {
+            //Construccion del query para obtener el Order, Line, Depto, DEPTO, WIDTH, HEIGHT, BALANCE
+            string Qry = "SELECT AADAT12.AADAT12.OPENSOPF3.ACBAC";
+
+        }
+
+
+    }
+}
